af67a13d20d8cc3375f4cdebdd62d9ee48fade76
enuel20 <emmaonana18@gmail.com>
2024-02-29 05:18:22 +0000

    simple unix command line interpreter

---
+++ betty_checker.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * betty: A function that passes the betty check
+ *
+ * Return: (0) if succesful
+ */
+
+int betty(void)
+{
+	printf("Nice betty complient code");
+	return (0);
+}
--- simple_shell.c
+++ simple_shell.c
@@ -1,44 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "main.h"
 
-#define BUFFER_SIZE 1024
-
-extern char **environ;
+/*
+ * Main: - Entry point of the simple shell program
+ *
+ * Description: This program implements a simple shell that reads
+ *              commands from the user, executes them using execve,
+ *              and waits for the command to complete.
+ *
+ * Return: 0 on successful execution, or exit(EXIT_FAILURE) on failure.
+ */
 
 int main(void)
 {
-	char buffer[BUFFER_SIZE];
+	char *line = NULL;
+	size_t buffer_size = 0;
 	ssize_t read_bytes;
 	char *args[2];
 
 	while (1)
 	{
 		printf("$ ");
-		read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-
-		if (read_bytes == 0)
+		read_bytes = getline(&line, &buffer_size, stdin);
+		if (read_bytes == -1)
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+		else if (read_bytes == 0)
 		{
 			printf("\n");
 			break;
 		}
-		else if (read_bytes == -1)
-		{
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
 		else
 		{
-			buffer[read_bytes - 1] = '\0';
-			args[0] = buffer;
+			if (line[read_bytes - 1] == '\n')
+			{
+				line[read_bytes - 1] = '\0';
+			}
+			args[0] = line;
 			args[1] = NULL;
-
 			if (fork() == 0)
 			{
-				if (execve(buffer, args, environ) == -1)
+				if (execve(line, args, environ) == -1)
 				{
-					perror(buffer);
+					perror(line);
 					exit(EXIT_FAILURE);
 				}
 			}
@@ -48,5 +53,7 @@ int main(void)
 			}
 		}
 	}
+	free(line);
 	return (0);
 }
+
